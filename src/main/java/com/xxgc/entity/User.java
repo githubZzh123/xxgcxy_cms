a141ebf41c435d297cc3c3bcfc030b26659213ca
githubZzh123 <3391330892@qq.com>
2024-10-09 09:59:15 +0800

    更改

---
+++ src/main/java/com/xxgc/entity/User.java
@@ -0,0 +1,10 @@
+package com.xxgc.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+@Setter
+@Getter
+public class User {
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/com/xxgc/vo/ResponseResult.java
@@ -0,0 +1,13 @@
+package com.xxgc.vo;
+
+import lombok.Data;
+
+@Data
+public class ResponseResult {
+    private String code; //提示码
+    private String message;//提示信息
+    public ResponseResult(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
